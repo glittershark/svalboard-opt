ff053c878caaad5c4d42de997f848ae5b7724baf
Catherine Van West <cat@khz.ac>
2025-05-18 18:28:13 -0400

    Filter letters, digraphs, trigraphs
    Right now this is regex-based because I'm lazy.

--- letter_weights.py
+++ letter_weights.py
@@ -3,6 +3,7 @@
 from dataclasses import dataclass
 
 import os
+import re
 
 
 @dataclass
@@ -22,7 +23,9 @@ class LetterWeights:
         letters="1-grams.txt",
         digraphs="2-grams.txt",
         trigraphs="3-grams.txt",
+        regex="[a-z ]+",
     ):
+        ex = re.compile(regex)
 
         l = LetterWeights()
         for p in [
@@ -30,12 +33,12 @@ class LetterWeights:
             [l.digraphs, digraphs],
             [l.trigraphs, trigraphs],
         ]:
-            p[0] |= LetterWeights._ingest(os.path.join(directory, p[1]))
+            p[0] |= LetterWeights._ingest(os.path.join(directory, p[1]), ex)
 
         return l
 
     @staticmethod
-    def _ingest(path):
+    def _ingest(path: str, ex: re.Pattern):
         contents = None
         with open(path, "r") as f:
             contents = f.read()
@@ -44,6 +47,9 @@ class LetterWeights:
 
         for line in contents.splitlines():
             tokens = line.split(" ", maxsplit=1)
-            ngraphs[tokens[1]] = int(tokens[0])
+            graph = tokens[1]
+            freq = int(tokens[0])
+            if ex.fullmatch(graph):
+                ngraphs[graph] = freq
 
         return ngraphs
