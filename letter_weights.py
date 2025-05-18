d6d3a401a7a4f6649192050dce63b13b74cc2dab
Catherine Van West <cat@khz.ac>
2025-05-18 18:11:08 -0400

    Load letter weights from somewhere

--- letter_weights.py
+++ letter_weights.py
@@ -2,12 +2,48 @@
 
 from dataclasses import dataclass
 
+import os
+
 
 @dataclass
 class LetterWeights:
     letters: dict[str, int]  # map from letter to frequency
     digraphs: dict[str, int]  # map from letter pair to frequency
+    trigraphs: dict[str, int]  # map from letter triplet to frequency
+
+    def __init__(self):
+        self.letters = {}
+        self.digraphs = {}
+        self.trigraphs = {}
 
     @staticmethod
-    def load():
-        raise NotImplementedError
+    def load(
+        directory="./ngrams/eng_wiki_1m/",
+        letters="1-grams.txt",
+        digraphs="2-grams.txt",
+        trigraphs="3-grams.txt",
+    ):
+
+        l = LetterWeights()
+        for p in [
+            [l.letters, letters],
+            [l.digraphs, digraphs],
+            [l.trigraphs, trigraphs],
+        ]:
+            p[0] |= LetterWeights._ingest(os.path.join(directory, p[1]))
+
+        return l
+
+    @staticmethod
+    def _ingest(path):
+        contents = None
+        with open(path, "r") as f:
+            contents = f.read()
+
+        ngraphs: dict[str, int] = {}
+
+        for line in contents.splitlines():
+            tokens = line.split(" ", maxsplit=1)
+            ngraphs[tokens[1]] = int(tokens[0])
+
+        return ngraphs
